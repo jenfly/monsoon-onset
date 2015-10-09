e4806d808e98309d482cc3908dec7a0bb82626b1
Jennifer Walker <jenfly@gmail.com>
2015-10-09 11:10:19 -0700

    Plot timeseries for individual years

--- scratchpad.py
+++ scratchpad.py
@@ -4,17 +4,23 @@ sys.path.append('/home/jwalker/dynamics/python/atmos-read')
 
 import numpy as np
 import xray
+import pandas as pd
 import matplotlib.pyplot as plt
 
 import atmos as atm
 import merra
 
-datadir = '/home/jennifer/datastore/merra/daily/'
+#datadir = '/home/jennifer/datastore/merra/daily/'
+datadir = '/home/jwalker/eady/datastore/merra/daily/'
 filestr = 'merra_uv200_40E-120E_60S-60N_'
 pathstr = datadir + filestr
 years = np.arange(1979, 2015)
 lon1, lon2 = 60, 100
-lat1, lat2 = 10, 30
+#lat1, lat2 = 10, 30
+lat1, lat2 = 10.625, 10.625
+#lat1, lat2 = 0.625, 0.625
+#lat1, lat2 = -5.625, -5.625
+#lat1, lat2 = -10.625, -10.625
 
 # ----------------------------------------------------------------------
 def timeseries_allyears(pathstr, years, lat1, lat2, lon1, lon2):
@@ -50,19 +56,27 @@ def timeseries_allyears(pathstr, years, lat1, lat2, lon1, lon2):
     return dsbar
 
 
+def plot_timeseries_year(dsbar, year, nroll=None):
+    iplot = {'U' : 1, 'V' : 2, 'rel_vort' : 3, 'Ro' : 4}
+    plt.figure(figsize=(12, 9))
+    plt.suptitle(year)
+    for nm in dsbar.data_vars:
+        var = dsbar[nm].sel(Year=year)        
+        plt.subplot(2, 2, iplot[nm])
+        plt.plot(var.Day, var, color='gray')
+        if nroll is not None:
+            data = pd.rolling_mean(np.squeeze(var.values), nroll)
+            plt.plot(var.Day, data, color='black')
+        plt.title(nm)
+    
+       
 # ----------------------------------------------------------------------
 
 dsbar = timeseries_allyears(pathstr, years, lat1, lat2, lon1, lon2)
 
-#y = 0
-year = 1979
-iplot = {'U' : 1, 'V' : 2, 'rel_vort' : 3, 'Ro' : 4}
-plt.figure(figsize=(12, 9))
-for nm in dsbar.data_vars:
-    #var = dsbar[nm][y]
-    var = dsbar[nm].sel(Year=year)
-    plt.subplot(2, 2, iplot[nm])
-    plt.plot(var.Day, var)
-    plt.title(nm)
+nroll = 10
+for year in [1979, 1980, 1981, 1982]:
+    plot_timeseries_year(dsbar, year, nroll)
+
 
 # ----------------------------------------------------------------------
