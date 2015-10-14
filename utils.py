c0f86e82e3263562aace90b9b53fdb98c1674bc4
Jennifer Walker <jenfly@gmail.com>
2015-10-14 16:22:45 -0700

    Added slice_premidpost(), composite_premidpost()

--- utils.py
+++ utils.py
@@ -8,3 +8,28 @@ import matplotlib.pyplot as plt
 
 import atmos as atm
 import merra
+
+def slice_premidpost(imid, n):
+    n1 = n // 2
+    n2 = n - n1
+    inds = {}
+    inds['pre'] = slice(imid - n1 - n, imid - n1)
+    inds['mid'] = slice(imid - n1, imid + n2)
+    inds['post'] = slice(imid + n2, imid + n2 + n)
+    return inds
+
+def composite_premidpost(data, onset, n):
+    dims = list(data.shape)
+    dims[1] = n
+    comp = {}
+    comp['pre'] = np.zeros(dims, dtype=float)
+    comp['mid'] = np.zeros(dims, dtype=float)
+    comp['post'] = np.zeros(dims, dtype=float)
+
+    for y, d in enumerate(onset):
+        inds = slice_premidpost(d - 1, n)
+        for key in comp:
+            comp[key][y] = data[y, inds[key]]
+    for key in comp:
+        comp[key][y] = comp[key][y].mean(axis=1)
+    return comp
