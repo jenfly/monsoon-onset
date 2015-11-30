a835f6dcd85fd70b37deb5c5a3acd4cfa3e9956f
Jennifer Walker <jenfly@gmail.com>
2015-11-30 11:08:18 -0800

    Added animations of daily data

--- testing/testing-utils-composites.py
+++ testing/testing-utils-composites.py
@@ -17,47 +17,102 @@ from utils import daily_rel2onset, comp_days_centered, composite
 
 # ----------------------------------------------------------------------
 datadir = atm.homedir() + 'datastore/merra/daily/'
-datafile = datadir + 'merra_u850_40E-120E_60S-60N_apr-sep_1979-2014.nc'
-with xray.open_dataset(datafile) as ds:
+onsetfile = datadir + 'merra_u850_40E-120E_60S-60N_apr-sep_1979-2014.nc'
+datafile = datadir + 'merra_precip_40E-120E_60S-60N_days91-274_1979-2014.nc'
+
+# Onset day calcs
+with xray.open_dataset(onsetfile) as ds:
     u = ds['U'].load()
-
-
 oci = indices.onset_OCI(u)
 d_onset = oci['onset']
-ndays = 30
-data = daily_rel2onset(u, d_onset, ndays, ndays)
 
+# Daily data fields
+var = 'precip'
+yearnm, daynm = 'year', 'day'
+cmap = 'hot_r'
+axlims = (-10, 50, 40, 120)
+with xray.open_dataset(datafile) as ds:
+    data = ds['PRECTOT'].load()
+
+# Align daily data relative to onset day
+ndays = 30
+data = daily_rel2onset(data, d_onset, ndays, ndays, yearnm=yearnm, daynm=daynm)
+years = data[yearnm].values
+databar = data.mean(dim=yearnm)
+
+# Days for pre/onset/post composites
 compdays = comp_days_centered(5)
 #compdays = comp_days_centered(1, 9)
 
-comp = composite(data, compdays)
-
+# Calculate composites
+comp = composite(data, compdays, daynm=daynm + 'rel')
 compbar = collections.OrderedDict()
 for key in comp:
-    compbar[key] = comp[key].mean(dim='Year')
+    compbar[key] = comp[key].mean(dim=yearnm)
 
-# Pre / onset / post composites
-axlims = (-60, 60, 40, 120)
+# Plot pre/onset/post composites - climatology
+cmin, cmax = 0, 20
 plt.figure(figsize=(12,10))
 for i, key in enumerate(comp):
     plt.subplot(2, 2, i+1)
-    atm.pcolor_latlon(compbar[key], axlims=axlims)
-    plt.title(key)
+    atm.pcolor_latlon(compbar[key], axlims=axlims, cmap=cmap)
+    plt.clim(cmin, cmax)
+    plt.title(var + ' ' + key)
 
-# Animation of climatological daily data relative to onset
-databar = data.mean(dim='Year')
-cmax = 20
+# Plot pre/onset/post composites - individual years
+cmin, cmax = 0, 30
+nrow, ncol = 3, 4
+figsize = (14, 10)
+for key in comp.keys():
+    suptitle = var + ' ' + key
+    for y, year in enumerate(years):
+        if y % (nrow * ncol) == 0:
+            fig, axes = plt.subplots(nrow, ncol, figsize=figsize, sharex=True)
+            plt.subplots_adjust(left=0.08, right=0.95, wspace=0, hspace=0.2)
+            plt.suptitle(suptitle)
+            yplot = 1
+        else:
+            yplot += 1
+
+        plt.subplot(nrow, ncol, yplot)
+        atm.pcolor_latlon(comp[key][y], axlims=axlims, cmap=cmap)
+        plt.clim(cmin, cmax)
+        plt.title(year)
+
+# ----------------------------------------------------------------------
+# Animation of daily data relative to onset
+
+nframes = 2*ndays + 1
+fps = 4
 
 def animate(i):
     plt.clf()
-    m, _ = atm.pcolor_latlon(databar[i], axlims=axlims)
-    plt.clim(-cmax, cmax)
-    plt.title(databar.Dayrel.values[i])
+    m, _ = atm.pcolor_latlon(animdata[i], axlims=axlims, cmap=cmap)
+    plt.clim(cmin, cmax)
+    day = animdata[daynm + 'rel'].values[i]
+    plt.title('%s %s RelDay %d' % (var, yearstr, day))
     return m
 
-nframes = 2*ndays + 1
+# Climatology
+cmin, cmax = 0, 20
+yearstr = '%d-%d' % (years[0], years[-1])
+animdata = databar
+savefile = 'mp4/' + var + '_climatology.mp4'
 fig = plt.figure()
 anim = animation.FuncAnimation(fig, animate, frames=nframes)
+anim.save(savefile, writer='mencoder', fps=fps)
+
+# Individual years
+cmin, cmax = 0, 60
+for y, year in enumerate(years):
+    print(year)
+    animdata = data[y]
+    yearstr = '%d' % year
+    savefile = 'mp4/%s_%s.mp4' % (var, yearstr)
+    plt.close('all')
+    fig = plt.figure()
+    anim = animation.FuncAnimation(fig, animate, frames=nframes)
+    anim.save(savefile, writer='mencoder', fps=fps)
 
 # # ----------------------------------------------------------------------
 # # Define monsoon onset index
