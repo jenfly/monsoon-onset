f26ef27215391aae29e3a6da0387780b5e62dd6a
Jennifer Walker <jenfly@gmail.com>
2016-02-03 21:20:01 -0800

    Added climatological mean

--- scripts/compare-years.py
+++ scripts/compare-years.py
@@ -27,21 +27,19 @@ comp_yrs['early'] = [2004, 1999, 1990, 2000, 2001]
 comp_yrs['late'] = [1983, 1992, 1997, 2014, 2012]
 savestr = 'early_late'
 
-datadir = atm.homedir() + 'datastore/merra/daily/'
+clim_yearstr = '1979-2014'
+datadir = atm.homedir() + 'datastore/merra/analysis/'
 savedir = 'figs/'
 
 varnms = ['precip', 'U200', 'V200', 'rel_vort200', 'Ro200',
            'abs_vort200', 'H200', 'T200',
            'U850', 'V850', 'H850', 'T850', 'QV850',
-           'T950', 'H950', 'QV950', 'V950', 'THETA950', 'THETA_E950',
-           'V*THETA_E950']
-
-keys_remove = ['T950', 'H950', 'QV950', 'V950',  'DSE950',
-                'MSE950', 'V*DSE950', 'V*MSE950']
+           'THETA950', 'THETA_E950', 'V*THETA_E950',
+           'HFLUX', 'EFLUX']
 
 # Day ranges for composites
-comp_keys = ['pre4', 'pre3', 'pre2', 'pre1']
-#comp_keys = ['post1', 'post2', 'post3', 'post4']
+#comp_keys = ['pre4', 'pre3', 'pre2', 'pre1']
+comp_keys = ['post1', 'post2', 'post3', 'post4']
 
 compdays_all = {'pre4' : np.arange(-60, -45),
                 'pre3' : np.arange(-45, -30),
@@ -63,81 +61,44 @@ lat1, lat2 = 10, 30
 # ----------------------------------------------------------------------
 # List of data files
 
-def yrlyfile(var, plev, year, subset=''):
-    return 'merra_%s%d_40E-120E_60S-60N_%s%d.nc' % (var, plev, subset, year)
+def data_filenames(years, varnms, onset_nm, datadir):
+    filestr = datadir + 'merra_%s_dailyrel_%s_%d.nc'
+    files = {}
+    for nm in varnms:
+        files[nm] = [filestr % (nm, onset_nm, yr) for yr in years]
+    return files
 
-def get_filenames(years, datadir):
-    datafiles = {}
-    datafiles['HOWI'] = ['merra_vimt_ps-300mb_%d.nc' % yr for yr in years]
-    datafiles['CHP_MFC'] = ['merra_MFC_ps-300mb_%d.nc' % yr for yr in years]
-    datafiles['CHP_PCP'] = ['merra_precip_%d.nc' % yr for yr in years]
-    datafiles['precip'] = datafiles['CHP_PCP']
+def clim_filenames(yearstr, varnms, onset_nm, datadir):
+    filestr = datadir + 'merra_%s_dailyrel_%s_%s.nc'
+    files = {nm : filestr % (nm, onset_nm, yearstr) for nm in varnms}
+    return files
 
-    for plev in [200, 850]:
-        files = [yrlyfile('uv', plev, yr) for yr in years]
-        for key in ['U', 'V', 'Ro', 'rel_vort', 'abs_vort']:
-            datafiles['%s%d' % (key, plev)] = files
-        for key in ['T', 'H', 'QV']:
-            key2 = '%s%d' % (key, plev)
-            datafiles[key2] = [yrlyfile(key, plev, yr) for yr in years]
+datafiles = {key: data_filenames(comp_yrs[key], varnms, onset_nm, datadir)
+             for key in comp_yrs}
+datafiles['clim'] = clim_filenames(clim_yearstr, varnms, onset_nm, datadir)
 
-    for plev in [950, 975]:
-        for key in ['T', 'H','QV', 'V']:
-            files = [yrlyfile(key, plev, yr) for yr in years]
-            datafiles['%s%d' % (key, plev)] = files
-
-    for varnm in datafiles:
-        files = datafiles[varnm]
-        datafiles[varnm] = [datadir + filenm for filenm in files]
-
-    return datafiles
-
-datafiles = {key: get_filenames(comp_yrs[key], datadir) for key in comp_yrs}
 
 # ----------------------------------------------------------------------
-# Calculate onset indices and get daily data
+# Get daily data relative to onset day
 
-def all_data(onset_nm, varnms, years, datafiles, npre, npost):
+data = {}
+for key in datafiles:
+    data[key] = {}
+    for nm in datafiles[key]:
+        ds = atm.load_concat(datafiles[key][nm], concat_dim='year')
+        varid = ds.data_vars.keys()[0]
+        var = ds[varid]
+        if 'year' in var.dims:
+            var = var.mean(dim='year')
+        data[key][nm] = var
 
-    # Monsoon onset day and index timeseries
-    index = utils.get_onset_indices(onset_nm, datafiles[onset_nm], years)
-    onset = index['onset']
-
-    # Read daily data fields and align relative to onset day
-    yearnm, daynm = 'year', 'day'
-    data = collections.OrderedDict()
-    for varnm in varnms:
-        print('Reading daily data for ' + varnm)
-        data[varnm] = utils.get_data_rel(varnm, years, datafiles.get(varnm), 
-                                         data, onset, npre, npost)
-    return index, data
-
-npre, npost = 90, 150
-
-data = collections.OrderedDict()
-index = collections.OrderedDict()
-for key in comp_yrs:
-    index[key], data[key] = all_data(onset_nm, varnms, comp_yrs[key],
-                                     datafiles[key], npre, npost)
-
-    # Mean over years for each composite
-    for nm in data[key]:
-        data[key][nm] = data[key][nm].mean(dim='year')
 
 # ----------------------------------------------------------------------
 # Housekeeping
 
-# Remove data that I don't want to include in plots
-for key1 in comp_yrs:
-    keys = data[key1].keys()
-    for key in keys_remove:
-        if key in keys:
-            data[key1] = atm.odict_delete(data[key1], key)
-
-
 # Fill Ro200 with NaNs near equator
 varnm = 'Ro200'
-for key1 in comp_yrs:
+for key1 in data:
     if varnm in data[key1]:
         latbuf = 5
         lat = atm.get_coord(data[key1][varnm], 'lat')
@@ -150,8 +111,8 @@ for key1 in comp_yrs:
 # Sector mean data
 
 lonname, latname = 'XDim', 'YDim'
-sectordata = collections.OrderedDict()
-for key1 in comp_yrs:
+sectordata = {}
+for key1 in data:
     sectordata[key1] = collections.OrderedDict()
     for varnm in data[key1]:
         var = atm.subset(data[key1][varnm], {lonname : (lon1, lon2)})
@@ -170,30 +131,23 @@ def plusminus(num):
 # ----------------------------------------------------------------------
 # Composite averages
 print('Computing composites relative to onset day')
-nms =  data[data.keys()[0]].keys()
-
-comp = collections.OrderedDict()
-sectorcomp = collections.OrderedDict()
-for nm in nms:
-    comp[nm] = collections.OrderedDict()
-    sectorcomp[nm] = collections.OrderedDict()
+comp = {}
+for nm in varnms:
+    comp[nm] = {}
     for key in data:
-        compdat = utils.composite(data[key][nm], compdays_all, daynm='dayrel',
-                                  return_avg=True)
-        compsec = utils.composite(sectordata[key][nm], compdays_all,
+        compdat = utils.composite(sectordata[key][nm], compdays_all,
                                   daynm='dayrel', return_avg=True)
         for dkey in compdat:
             key2 = key + '_' + dkey
             comp[nm][key2] = compdat[dkey]
-            sectorcomp[nm][key2] = compsec[dkey]
 
 # Get max/min values from all composites for setting consistent
 # ylimits on plots
 ylimits = {}
-for nm in nms:
-    for i, key in enumerate(sectorcomp[nm]):
-        val1 = sectorcomp[nm][key].min().values
-        val2 = sectorcomp[nm][key].max().values
+for nm in varnms:
+    for i, key in enumerate(comp[nm]):
+        val1 = comp[nm][key].min().values
+        val2 = comp[nm][key].max().values
         if i == 0:
             ylim1, ylim2 = val1, val2
         else:
@@ -213,13 +167,20 @@ for key in compdays:
     d2 = plusminus(compdays[key].max())
     compnms[key] = 'D0%s:D0%s' % (d1, d2)
 
+suptitle = '%d-%dE Composites Relative to %s Onset Day' % (lon1, lon2, onset_nm)
 ncol = len(compdays.keys())
 nrow = 4
 figsize = (12, 9)
-suptitle = '%d-%dE Composites Relative to %s Onset Day' % (lon1, lon2, onset_nm)
+xlimits = (-60, 60)
+fmt = {'early' : 'k--', 'late' : 'k', 'clim' : 'k'}
+lwidth = {'early' : 1, 'late' : 1, 'clim' : 2}
+alph = {'early' : 1, 'late' : 1, 'clim' : 0.4}
+if 'pre1' in compdays:
+    legend_loc = 'upper right'
+else:
+    legend_loc = 'upper left'
 
-fmt_str = {'early' : 'k--', 'late' : 'k'}
-for i, nm in enumerate(sectorcomp):
+for i, nm in enumerate(varnms):
     if i % nrow == 0:
         row = 1
         fig, axes = plt.subplots(nrow, ncol, figsize=figsize, sharex=True,
@@ -230,17 +191,13 @@ for i, nm in enumerate(sectorcomp):
         row += 1
     for j, dkey in enumerate(compdays):
         ax = axes[row - 1, j]
-        ### Temporary - put real climatology in here
-        lat = atm.get_coord(sectorcomp[nm]['early_' + dkey], 'lat')
-        varbar = 0.5 * (sectorcomp[nm]['early_' + dkey] +
-                        sectorcomp[nm]['late_' + dkey])
-        ax.plot(lat, varbar, color='k', linewidth=2, alpha=0.3, label='clim')
-        ###
-        for yrkey in comp_yrs:
-            var = sectorcomp[nm][yrkey + '_' + dkey]
+        for yrkey in ['clim'] + comp_yrs.keys():
+            var = comp[nm][yrkey + '_' + dkey]
             lat = atm.get_coord(var, 'lat')
-            ax.plot(lat, var, fmt_str[yrkey], label=yrkey)
+            ax.plot(lat, var, fmt[yrkey], linewidth=lwidth[yrkey],
+                    alpha=alph[yrkey], label=yrkey)
         ax.grid(True)
+        ax.set_xlim(xlimits)
         ax.set_ylim(ylimits[nm])
         if row == 1:
             ax.set_title(compnms[dkey])
@@ -249,4 +206,4 @@ for i, nm in enumerate(sectorcomp):
         if j == 0:
             ax.set_ylabel(nm)
         if i == 0 and j == 0:
-            ax.legend(fontsize=8, loc='upper left')
+            ax.legend(fontsize=10, loc=legend_loc)
