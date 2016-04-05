0b40eeeedb0056f377b34a3c0dc9a5edf6d69a98
Jennifer Walker <jenfly@gmail.com>
2016-04-05 16:34:37 -0700

    Plot regression coefficients

--- scripts/regression-analysis.py
+++ scripts/regression-analysis.py
@@ -24,11 +24,13 @@ datadir = atm.homedir() + 'datastore/merra/analysis/'
 varnms = ['U200', 'V200', 'T200', 'H200', 'U850', 'V850', 'T850', 'H850',
           'THETA950', 'THETA_E950', 'QV950', 'T950', 'HFLUX', 'EFLUX',
           'EVAP', 'precip']
-regdays = [-60, -30, 0, 30, 60]
-seasons = ['JJAS', 'SSN']
+regdays = [-60, -30, 0]
+seasons = ['JJAS']
 lon1, lon2 = 60, 100
 nroll = 5
 
+# ----------------------------------------------------------------------
+# Read data
 def get_filenames(datadir, varnms, onset_nm, years, lon1, lon2, nroll):
     lonstr = atm.latlon_str(lon1, lon2, 'lon')
     yearstr = '_%d-%d.nc' % (min(years), max(years))
@@ -47,23 +49,98 @@ reg = {}
 
 for varnm in varnms:
     for key in ['latlon', 'sector']:
-        with xray.open_dataset(datafiles[varnm][key]) as ds:
+        filenm = datafiles[varnm][key]
+        print('Loading ' + filenm)
+        with xray.open_dataset(filenm) as ds:
             reg[varnm + '_' + key] = ds.load()
 
+# ----------------------------------------------------------------------
+def stipple_mask(p):
+    return ((p >= 0.05) | np.isnan(p))
 
+def sector_plot(varnm, reg, coeff='m', stipple_kw={}, grp=None, ylim=None,
+                yticks=None):
+    xname, yname = 'dayrel', 'lat'
+    var = reg[varnm + '_sector'][coeff]
+    p = reg[varnm + '_sector']['p']
+    pts_mask = stipple_mask(p)
+    lat = atm.get_coord(var, 'lat')
+    days = atm.get_coord(var, 'dayrel')
+    xsample = 6
+    if max(np.diff(lat)) > 1:
+        ysample = 2
+    else:
+        ysample = 4
+    utils.contourf_lat_time(lat, days, var)
+    atm.stipple_pts(pts_mask, xname, yname, xsample, ysample, **stipple_kw)
+    plt.title(varnm)
+    if grp is not None:
+        if grp.col > 0:
+            plt.ylabel('')
+        if grp.row < grp.nrow - 1:
+            plt.xlabel('')
+    if ylim is not None:
+        plt.ylim(ylim)
+    if yticks is not None:
+        plt.yticks(yticks)
+            
 
-filenm = datadir + 'merra_U200_nroll7_reg_60E-100E_onset_CHP_MFC_1979-2014.nc'
-with xray.open_dataset(filenm) as ds:
-    ds.load()
+def latlon_plot(varnm, reg, day_or_season, coeff='m', stipple_kw={},
+                axlims=(-60, 60, 40, 120)):
+    regdata = reg[varnm + '_latlon']
+    keys = [key for key in regdata if key.endswith('_' + coeff)]
+    cint = atm.cinterval(regdata[keys].to_array())
+    clev = atm.clevels(regdata[keys].to_array(), cint, symmetric=True)
+    xname, yname = 'lon', 'lat'
+    xsample, ysample = 2, 2
+    if isinstance(day_or_season, int):
+        key = varnm + '_DAILY_'
+        var = regdata[key + coeff].sel(dayrel=day_or_season)
+        p = regdata[key + 'p'].sel(dayrel=day_or_season)
+        titlestr = varnm + ' Day %d' % day_or_season
+    else:
+        key = varnm + '_' + day_or_season + '_'
+        var = regdata[key + coeff]
+        p = regdata[key + 'p']
+        titlestr = varnm + ' ' + day_or_season
+    pts_mask = stipple_mask(p)
+    atm.contourf_latlon(var, clev=clev, axlims=axlims)
+    atm.stipple_pts(pts_mask, xname, yname, xsample, ysample, **stipple_kw)
+    plt.title(titlestr)
+    
 
-lat = atm.get_coord(ds, 'lat')
-days = atm.get_coord(ds, 'dayrel')
-var = ds['r']
-pts_mask = (ds['p'] >= 0.05)
-xsample, ysample = 4, 2
+# Latitude-day contour plots of sector mean
+nrow, ncol = 2, 2
+fig_kw = {'figsize' : (14, 10), 'sharex' : True, 'sharey' : True}
+gridspec_kw = {'left' : 0.05, 'right' : 0.95, 'bottom' : 0.05, 'top' : 0.95,
+               'wspace' : 0.05, 'hspace' : 0.1}
+stipple_kw = {'markersize' : 2, 'markeredgewidth' : 1.5}
+coeff = 'm'
+suptitle = '%d-%dE Reg onto %s Onset - Reg Coefficients' % (lon1, lon2, onset_nm)
+ylim, yticks = (-60, 60), np.arange(-60, 61, 20)
 
-xname, yname = 'dayrel', 'YDim'
+grp = atm.FigGroup(nrow, ncol, fig_kw=fig_kw, gridspec_kw=gridspec_kw,
+                   suptitle=suptitle)
+for varnm in varnms:
+    grp.next()
+    sector_plot(varnm, reg, coeff, stipple_kw, grp, ylim, yticks)
+  
 
-plt.figure()
-utils.contourf_lat_time(lat, days, var)
-atm.stipple_pts(pts_mask, xname, yname, xsample, ysample)
+# Lat-lon maps
+plt_list = regdays + seasons
+nrow = 2
+ncol = len(plt_list)
+fig_kw = {'figsize' : (14, 10), 'sharex' : True, 'sharey' : True}
+gridspec_kw = {'left' : 0.05, 'right' : 0.95, 'bottom' : 0.05, 'top' : 0.95,
+               'wspace' : 0.15, 'hspace' : 0.1}
+stipple_kw = {'markersize' : 2, 'markeredgewidth' : 1.5}
+coeff = 'm'
+suptitle = 'Reg onto %s Onset - Reg Coefficients' % onset_nm
+grp = atm.FigGroup(nrow, ncol, fig_kw=fig_kw, gridspec_kw=gridspec_kw,
+                   suptitle=suptitle)
+for varnm in varnms:
+    for day_or_season in plt_list:
+        grp.next()
+        latlon_plot(varnm, reg, day_or_season, coeff, stipple_kw)
+    
+
