d6cca09576d76b61d1681572918613ac5d6d0297
Jennifer Walker <jenfly@gmail.com>
2016-04-20 21:33:29 -0700

    Momentum budget on multiple pressure levels

--- scripts/save-dailyrel-momentum-budget.py
+++ scripts/save-dailyrel-momentum-budget.py
@@ -16,11 +16,14 @@ import utils
 # ----------------------------------------------------------------------
 onset_nm = 'CHP_MFC'
 years = np.arange(1979, 2015)
+plevs = [1000,925,850,775,700,600,500,400,300,250,150,100,70,50,30,20]
 datadir = atm.homedir() + 'datastore/merra/analysis/'
-filestr = datadir + 'merra_ubudget200_ndays5_60E-100E_%d.nc'
-savestr = datadir + 'merra_ubudget200_dailyrel_%s_ndays5_60E-100E' % onset_nm
-datafiles = [filestr % yr for yr in years]
-savefiles = [savestr  + '_%d.nc' % yr for yr in years]
+filestr = datadir + 'merra_ubudget%d_ndays5_60E-100E_%d.nc'
+savestr = datadir + 'merra_ubudget%d_dailyrel_' + onset_nm + '_ndays5_60E-100E'
+datafiles, savefiles = {}, {}
+for plev in plevs:
+    datafiles[plev] = [filestr % (plev, yr) for yr in years]
+    savefiles[plev] = [savestr % plev + '_%d.nc' % yr for yr in years]
 subset1 = '40E-120E_90S-90N'
 indfiles = ['%sdatastore/merra/daily/merra_MFC_%s_%d.nc' %
             (atm.homedir(), subset1, yr) for yr in years]
@@ -37,34 +40,34 @@ retreat = index['retreat'].values
 
 # ----------------------------------------------------------------------
 # Get daily data
-yearnm, daynm = 'year', 'day'
 
-for y, year in enumerate(years):
-    datafile = datafiles[y]
-    d_onset, d_retreat = onset[y], retreat[y]
-    ds_rel = xray.Dataset()
-    print('Loading ' + datafile)
-    with xray.open_dataset(datafiles[y]) as ds:
-        ds_rel.attrs = ds.attrs
-        for nm in ds.data_vars:
-            var = atm.expand_dims(ds[nm], yearnm, year)
-            ds_rel[nm] = utils.daily_rel2onset(var, d_onset, npre, npost,
-                                               daynm, yearnm)
-    ds_rel.attrs['d_onset'] = d_onset
-    ds_rel.attrs['d_retreat'] = d_retreat
-    savefile = savefiles[y]
-    print('Saving to ' + savefile)
-    ds_rel.to_netcdf(savefile)
+for plev in plevs:
+    for y, year in enumerate(years):
+        datafile = datafiles[plev][y]
+        d_onset, d_retreat = onset[y], retreat[y]
+        ds_rel = xray.Dataset()
+        print('Loading ' + datafile)
+        with xray.open_dataset(datafile) as ds:
+            ds_rel.attrs = ds.attrs
+            for nm in ds.data_vars:
+                var = atm.expand_dims(ds[nm], yearnm, year)
+                ds_rel[nm] = utils.daily_rel2onset(var, d_onset, npre, npost)
+        ds_rel.attrs['d_onset'] = d_onset
+        ds_rel.attrs['d_retreat'] = d_retreat
+        savefile = savefiles[plev][y]
+        print('Saving to ' + savefile)
+        ds_rel.to_netcdf(savefile)
 
 
 # ----------------------------------------------------------------------
 # Compute climatologies and save
 
 yearstr = '%d-%d' % (years.min(), years.max())
-relfiles = [savestr + '_%d.nc' % yr for yr in years]
-savefile = savestr + '_' + yearstr + '.nc'
-ds = atm.combine_daily_years(None, relfiles, years, yearname='year')
-ds = ds.mean(dim='year')
-ds.attrs['years'] = years
-print('Saving to ' + savefile)
-ds.to_netcdf(savefile)
+for plev in plevs:
+    relfiles = savefiles[plev]
+    savefile = savestr % plev + '_' + yearstr + '.nc'
+    ds = atm.combine_daily_years(None, relfiles, years, yearname='year')
+    ds = ds.mean(dim='year')
+    ds.attrs['years'] = years
+    print('Saving to ' + savefile)
+    ds.to_netcdf(savefile)
