f91ecaaa9d600fa417729c28c5998fc605d9990b
Jennifer Walker <jenfly@gmail.com>
2016-05-24 15:13:59 -0700

    Updated for MERRA2 and removed obsolete code

--- scripts/save-dailyrel.py
+++ scripts/save-dailyrel.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('/home/jwalker/dynamics/python/atmos-tools')
 sys.path.append('/home/jwalker/dynamics/python/atmos-read')
+sys.path.append('/home/jwalker/dynamics/python/monsoon-onset')
 
 import xray
 import numpy as np
@@ -14,17 +15,21 @@ from utils import get_data_rel, load_dailyrel
 
 # ----------------------------------------------------------------------
 version = 'merra2'
-years = np.arange(1980, 2010)
+years = np.arange(1980, 2016)
 onset_nm = 'CHP_MFC'
 ind_nm, npre, npost = 'onset', 120, 200
 #ind_nm, npre, npost = 'retreat', 200, 60
 
 datadir = atm.homedir() + 'datastore/%s/daily/' % version
-savedir = atm.homedir() + 'datastore/%s/analysis/' % version
+savedir = atm.homedir() + 'eady/datastore/%s/analysis/' % version
 yearstr = '%d-%d' % (min(years), max(years))
 indfile = savedir + version + '_index_%s_%s.nc' % (onset_nm, yearstr)
 
-varnms = ['precip', 'U200', 'V200', 'U850', 'V850', 'T200', 'T850']
+varnms = ['PRECTOT', 'U200', 'V200',  'T200', 'H200', 'OMEGA500',
+          'U850', 'V850', 'T850', 'H850', 'QV850', 'TLML', 'QLML', 'PS',
+          'EFLUX', 'EVAP', 'HFLUX', 'VFLXCPT', 'VFLXPHI', 'VFLXQV',
+          'UFLXCPT', 'UFLXPHI', 'UFLXQV']
+sector_varnms = ['U', 'V', 'OMEGA', 'T', 'H', 'QV']
 keys_remove = ['H950', 'V950',  'DSE950', 'MSE950', 'V*DSE950', 'V*MSE950']
 
 # Lat-lon box for MFC / precip onset calcs
@@ -34,66 +39,32 @@ lat1, lat2 = 10, 30
 # ----------------------------------------------------------------------
 # List of data files
 
-def yrlyfile(version, var, plev, year, subset1='40E-120E_90S-90N', subset2=''):
-    if plev is None:
-        varid = var
-    else:
-        varid = '%s%d' % (var, plev)
-    return '%s_%s_%s_%s%d.nc' % (version, varid, subset1, subset2, year)
+def yrlyfile(version, datadir, varnm, year, subset1='40E-120E_90S-90N',
+             subset2=''):
+    filenm = datadir + '%d/%s_%s_%s_%s%d.nc'
+    filenm = filenm % (year, version, varnm, subset1, subset2, year)
+    return filenm
 
-def get_savefile(savedir, varnm, onset_nm, ind_nm, year):
-    filenm = savedir + 'merra_%s_dailyrel' % varnm
+def get_savefile(version, savedir, varnm, onset_nm, ind_nm, year):
+    filenm = savedir + version + '_%s_dailyrel' % varnm
     if ind_nm == 'retreat':
         filenm = filenm + '_retreat'
     filenm = filenm + '_%s_%d.nc' % (onset_nm, year)
     return filenm
 
-def get_filenames(years, datadir):
-    datafiles = {}
-    datafiles['HOWI'] = ['merra_vimt_ps-300mb_%d.nc' % yr for yr in years]
-    #datafiles['CHP_MFC'] = ['merra_MFC_ps-300mb_%d.nc' % yr for yr in years]
-    subset1 = '40E-120E_90S-90N'
-    datafiles['CHP_MFC'] = [yrlyfile('MFC', None, yr, subset1) for yr in years]
-    datafiles['CHP_PCP'] = ['merra_precip_%d.nc' % yr for yr in years]
-    datafiles['precip'] = datafiles['CHP_PCP']
+# Lat-lon data files
+datafiles = {}
+for nm in varnms:
+    datafiles[nm] = [yrlyfile(version, datadir, nm, yr) for yr in years]
 
-    for plev in [200, 850]:
-        # files = [yrlyfile('uv', plev, yr) for yr in years]
-        # for key in ['U', 'V', 'Ro', 'rel_vort', 'abs_vort']:
-        #     datafiles['%s%d' % (key, plev)] = files
-        for key in ['U', 'V', 'T', 'H', 'QV']:
-            if key == 'T':
-                subset1 = '40E-120E_60S-60N'
-            else:
-                subset1 = '40E-120E_90S-90N'
-            key2 = '%s%d' % (key, plev)
-            datafiles[key2] = [yrlyfile(key, plev, yr, subset1) for yr in years]
-
-    for plev in [950, 975]:
-        for key in ['T', 'H','QV', 'V']:
-            files = [yrlyfile(key, plev, yr) for yr in years]
-            datafiles['%s%d' % (key, plev)] = files
-
-    for key in ['EFLUX', 'HFLUX', 'EVAP', 'VFLXPHI', 'VFLXCPT', 'VFLXQV',
-                'DUDTANA200', 'TLML', 'QLML', 'PS']:
-        subset1 = '40E-120E_90S-90N'
-        datafiles[key] = [yrlyfile(key, None, yr, subset1) for yr in years]
-
-    # Zonal and sector mean data
-    for lonlims in [(0, 360), (60, 100)]:
-        lonstr = atm.latlon_str(lonlims[0], lonlims[1], 'lon')
-        subset1 = 'sector_' + lonstr
-        for nm in ['U', 'V', 'OMEGA', 'T', 'H', 'QV']:
-            key = nm + '_' + subset1
-            datafiles[key] = [yrlyfile(nm, None, yr, subset1) for yr in years]
-
-    for varnm in datafiles:
-        files = datafiles[varnm]
-        datafiles[varnm] = [datadir + filenm for filenm in files]
-
-    return datafiles
-
-datafiles = get_filenames(years, datadir)
+# Zonal and sector mean data
+for lonlims in [(0, 360), (60, 100)]:
+    lonstr = atm.latlon_str(lonlims[0], lonlims[1], 'lon')
+    subset1 = 'sector_' + lonstr
+    for nm in sector_varnms:
+        key = nm + '_' + subset1
+        datafiles[key] = [yrlyfile(version, datadir, nm, yr, subset1)
+                          for yr in years]
 
 # ----------------------------------------------------------------------
 # Onset index for each year
@@ -106,25 +77,6 @@ retreat = index['retreat']
 # ----------------------------------------------------------------------
 # Get daily data
 
-def housekeeping(data, keys_remove):
-    # Remove intermediate data that I don't want to keep
-    keys = data.keys()
-    for key in keys_remove:
-        if key in keys:
-            data = atm.odict_delete(data, key)
-
-    # Fill Ro200 with NaNs near equator
-    varnm = 'Ro200'
-    if varnm in data:
-        latbuf = 5
-        lat = atm.get_coord(data[varnm], 'lat')
-        latbig = atm.biggify(lat, data[varnm], tile=True)
-        vals = data[varnm].values
-        vals = np.where(abs(latbig)>latbuf, vals, np.nan)
-        data[varnm].values = vals
-    return data
-
-
 def get_varid(varnm):
     if varnm.find('sector') >= 0:
         varid = varnm.split('_')[0]
@@ -133,39 +85,31 @@ def get_varid(varnm):
     return varid
 
 
-
 # Save daily data for each year to file
-yearnm, daynm = 'year', 'day'
-relfiles = {}
 for y, year in enumerate(years):
     files = {key : [datafiles[key][y]] for key in datafiles}
     d_onset = int(onset[y].values)
     d_retreat = int(retreat[y].values)
     d0 = int(index[ind_nm][y].values)
-    coords = {yearnm : [year]}
+    coords = {'year' : [year]}
     onset_var = xray.DataArray([d_onset], name='D_ONSET', coords=coords)
     retreat_var = xray.DataArray([d_retreat], name='D_RETREAT', coords=coords)
-    data = {}
-    for varnm in varnms:
+    for varnm in files:
         print('Reading daily data for ' + varnm)
         varid = get_varid(varnm)
-        var = get_data_rel(varid, year, files.get(varnm), data, d0,
-                           npre, npost, yearnm, daynm)
+        var = get_data_rel(varid, year, files.get(varnm), data, d0, npre, npost)
         var.attrs = atm.odict_delete(var.attrs, 'd_onset')
         var.name = varid
-        data[varnm] = var
-    data = housekeeping(data, keys_remove)
-    for varnm in data:
-        savefile = get_savefile(savedir, varnm, onset_nm, ind_nm, year)
-        if y == 0:
-            relfiles[varnm] = [savefile]
-        else:
-            relfiles[varnm] = relfiles[varnm] + [savefile]
+        savefile = get_savefile(version, savedir, varnm, onset_nm, ind_nm, year)
         print('Saving to ' + savefile)
-        atm.save_nc(savefile, data[varnm], onset_var, retreat_var)
+        atm.save_nc(savefile, var, onset_var, retreat_var)
 
 # ----------------------------------------------------------------------
 # Compute climatologies and save
+relfiles = {}
+for key in datafiles:
+    relfiles[key] = [get_savefile(version, savedir, key, onset_nm, ind_nm, yr)
+                     % yr for yr in years]
 
 for varnm in relfiles:
     varid = get_varid(varnm)
