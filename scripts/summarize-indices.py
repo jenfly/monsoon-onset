294304a7e5d0300be79f31eef90906842b6eca14
Jennifer Walker <jenfly@gmail.com>
2016-05-25 15:43:04 -0700

    Update for merra2 and use saved timeseries from index files

--- scripts/summarize-indices.py
+++ scripts/summarize-indices.py
@@ -16,55 +16,26 @@ import utils
 mpl.rcParams['font.size'] = 11
 
 # ----------------------------------------------------------------------
+version = 'merra2'
+years = np.arange(1980, 2016)
 onset_nm = 'CHP_MFC'
-years = np.arange(1979, 2015)
-datadir = atm.homedir() + 'datastore/merra/daily/'
-datafiles = {}
-filestr = datadir + 'merra_%s_%s_%d.nc'
-datafiles['CHP_MFC'] = [filestr % ('MFC', '40E-120E_90S-90N', y) for y in years]
-datafiles['HOWI'] = [filestr % ('vimt', 'ps-300mb', y) for y in years]
-datafiles['OCI'] = [filestr % ('uv850', '40E-120E_60S-60N', y) for y in years]
-datafiles['SJKE'] = datafiles['OCI']
-
-# Lat-lon box
-lon1, lon2 = 60, 100
-lat1, lat2 = 10, 30
+datadir = atm.homedir() + 'datastore/%s/analysis/' % version
+yearstr = '%d-%d.nc' % (min(years), max(years))
+filestr = datadir + version + '_index_%s_' + yearstr
+datafiles = {nm : filestr % nm for nm in ['CHP_MFC', 'HOWI', 'OCI']}
+datafiles['MOK'] = atm.homedir() + 'dynamics/python/monsoon-onset/data/MOK.dat'
 
 # ----------------------------------------------------------------------
-# Read data and calculate onset/retreat indices
-index = collections.OrderedDict()
+# Read data
+index = {}
 
-# Accumulated MFC changepoint (Cook & Buckley 2009)
-mfc = atm.combine_daily_years('MFC', datafiles['CHP_MFC'], years,
-                              yearname='year')
-mfcbar = atm.mean_over_geobox(mfc, lat1, lat2, lon1, lon2)
-mfc_acc = np.cumsum(mfcbar, axis=1)
-index['CHP_MFC'] = indices.onset_changepoint(mfc_acc)
-index['CHP_MFC']['daily_rates'] = mfcbar
-
-# HOWI index (Webster and Fasullo 2003)
-npts, maxbreak = 100, 10
-ds = atm.combine_daily_years(['uq_int', 'vq_int'], datafiles['HOWI'], years,
-                             yearname='year')
-index['HOWI'], _ = indices.onset_HOWI(ds['uq_int'], ds['vq_int'], npts,
-                                   maxbreak=maxbreak)
-
-# 850 mb U, V for OCI and SJKE
-ds = atm.combine_daily_years(['U', 'V'], datafiles['OCI'], years)
-ds = ds.rename({'Year' : 'year', 'Day' : 'day'})
-u850, v850 = atm.squeeze(ds['U']), atm.squeeze(ds['V'])
-
-# OCI index (Wang et al 2009)
-index['OCI'] = indices.onset_OCI(u850, yearnm='year', daynm='day')
-
-# SJKE index (Boos and Emmanuel 2009)
-thresh_std = 0.7
-nroll = 7
-u_sm = atm.rolling_mean(u850, nroll, axis=1, center=True)
-v_sm = atm.rolling_mean(v850, nroll, axis=1, center=True)
-index['SJKE'] = indices.onset_SJKE(u_sm, v_sm, thresh_std=thresh_std,
-                                   yearnm='year', daynm='day')
-index['SJKE'].attrs['nroll'] = nroll
+for nm in datafiles:
+    if nm == 'MOK':
+        mok = indices.onset_MOK(datafiles['MOK'], yearsub=years)
+        index['MOK'] =  xray.Dataset({'onset' : mok})
+    else:
+        with xray.open_dataset(datafiles[nm]) as ds:
+            index[nm] = ds.load()
 
 # ----------------------------------------------------------------------
 # Plot daily timeseries and fit in a single year
@@ -106,7 +77,7 @@ for nm in onset.columns:
 
 styles = {'CHP_MFC' : {'color' : 'k', 'linewidth' : 2},
           'OCI' : {'color' : 'r'}, 'HOWI' : {'color' : 'b'},
-          'SJKE' : {'color' : 'g'}}
+          'MOK' : {'color' : 'g'}}
 xticks = np.arange(1980, 2016, 5)
 xticklabels = [1980, '', 1990, '', 2000, '', 2010, '']
 
