c0428b560d881517850bf838165df18528907d9a
Jennifer Walker <jenfly@gmail.com>
2016-05-27 18:50:00 -0700

    Added option for daily GPCP

--- scripts/save-dailyrel-precip.py
+++ scripts/save-dailyrel-precip.py
@@ -16,17 +16,23 @@ import precipdat
 
 # ----------------------------------------------------------------------
 version, yearstr = 'merra2', '1980-2015'
-onset_nm = 'CHP_MFC'
-years = np.arange(1980, 2015) # CMAP last full year is 2014
-
 datadir = atm.homedir() + 'datastore/%s/analysis/' % version
-pcpfile = atm.homedir() + 'datastore/cmap/cmap.enhanced.precip.pentad.mean.nc'
+onset_nm = 'CHP_MFC'
 indfile = atm.homedir() + ('datastore/%s/analysis/%s_index_%s_%s.nc' %
                            (version, version, onset_nm, yearstr))
-ind_nm, npre, npost = 'onset', 119, 200
+ind_nm, npre, npost = 'onset', 120, 200
+#ind_nm, npre, npost = 'onset', 119, 200
 #ind_nm, npre, npost = 'retreat', 200, 50
-savefile = datadir + 'cmap_dailyrel_%s_%s-%d.nc' % (onset_nm, min(years),
-                                                    max(years))
+
+pcp_nm = 'gpcp'
+years = np.arange(1997, 2015)
+#years = np.arange(1980, 2015) # CMAP last full year is 2014
+#pcpfiles = atm.homedir() + 'datastore/cmap/cmap.enhanced.precip.pentad.mean.nc'
+pcpfiles = [atm.homedir() + 'datastore/gpcp/gpcp_daily_%d.nc' % yr
+            for yr in years]
+
+savefile = datadir + '%s_dailyrel_%s_%s-%d.nc' % (pcp_nm, onset_nm, min(years),
+                                                  max(years))
 if ind_nm == 'retreat':
     savefile = savefile.replace('dailyrel', 'dailyrel_retreat')
 # ----------------------------------------------------------------------
@@ -39,18 +45,22 @@ index = index.sel(year=years)
 d0 = index[ind_nm].values
 
 # Precip data
-pcp = precipdat.read_cmap(pcpfile, yearmin=min(years), yearmax=max(years))
-name, attrs, coords, dimnames = atm.meta(pcp)
+if pcp_nm == 'cmap':
+    pcp = precipdat.read_cmap(pcpfiles, yearmin=min(years), yearmax=max(years))
 
-# Interpolate to daily resolution
-days = np.arange(3, 364)
-interp_func = scipy.interpolate.interp1d(pcp['day'], pcp, axis=1)
-vals = interp_func(days)
-coords['day'] = xray.DataArray(days, coords={'day' : days})
-pcp_i = xray.DataArray(vals, dims=dimnames, coords=coords, name=name,
-                       attrs=attrs)
+    # Interpolate to daily resolution
+    name, attrs, coords, dimnames = atm.meta(pcp)
+    days = np.arange(3, 364)
+    interp_func = scipy.interpolate.interp1d(pcp['day'], pcp, axis=1)
+    vals = interp_func(days)
+    coords['day'] = xray.DataArray(days, coords={'day' : days})
+    pcp = xray.DataArray(vals, dims=dimnames, coords=coords, name=name,
+                         attrs=attrs)
+else:
+    pcp = atm.combine_daily_years(None, pcpfiles, years, yearname='year')
+
 
 # Daily relative to onset/withdrawal
-pcp_rel = utils.daily_rel2onset(pcp_i, d0, npre, npost)
+pcp_rel = utils.daily_rel2onset(pcp, d0, npre, npost)
 print('Saving to ' + savefile)
 atm.save_nc(savefile, pcp_rel)
